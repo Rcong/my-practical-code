b191efc1fc0b95ecbffc264466a138c53703593f
Rcong <654846387@qq.com>
2016-02-17 21:44:26 +0800

    commit

---
+++ vue-loader-demo/src/app.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <img class="logo" src="./assets/logo.png"/>
+    <h1>{{msg}}></h1>
+    <comp-a></comp-a>
+    <comp-b></comp-b>
+    <counter></counter>
+  </div>
+</template>
+
+<script>
+import CompA from './components/a.vue'
+import CompB from './components/b.vue'
+import Counter from './components/counter.vue'
+export default {
+  data () {
+    return {
+      msg: 'Hello from vue-loader!'
+    }
+  },
+  components: {
+    CompA,
+    CompB,
+    Counter
+  }
+}
+</script>
+
+<style>
+
+.logo{
+  width:40px;
+  height:40px;
+}
+
+</style>
---
+++ vue-loader-demo/src/components/a.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    <h2 class="red">{{msg}}</h2>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        msg:'Hello from component A!'
+      }
+    }
+  }
+</script>
+
+<style>
+  .container{
+    border:1px solid #00f;
+  }
+  .red{
+    color:#f00;
+  }
+</style>
